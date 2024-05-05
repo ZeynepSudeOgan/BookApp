4fc6e3c177e7ac9433d879eb86aead9b76bc8eca
zeynep sude <150245935+ZeynepSudeOgan@users.noreply.github.com>
2024-05-05 05:52:02 +0300

    Update book_detail_page.dart
    bookdetail denedim

--- lib/pages/book_detail_page.dart
+++ lib/pages/book_detail_page.dart
@@ -20,8 +20,71 @@ class BookDetailPage extends StatelessWidget {
       future: getData(),
       builder: (context, value) => Scaffold(
         appBar: AppBar(),
-        body: Center(
-          child: Text("bookId:${book?.name ?? "null"}"),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                          "https://www.canyayinlari.com/productimages/117775/original/9789750715709_735.jpg",
+                          width: 160,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 15),
+                Text("bookId:${book!.name ?? "null"}"),
+                const SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text("Release Date:${book!.releaseDate}"),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text("Author:${book!.authorId}"),
+                      ], //yapamadımm ama yşne de yazdım bir şeyler :(
+                    ),
+                    Column(
+                      children: [Text("Rating:${book!.totalVote}")],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Row(
+                        children: [
+                          Text("About Book"),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      Row(
+                        children: [
+                          Flexible(
+                              child: Text("description:${book!.description}"))
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                const Text("Auther")
+              ],
+            ),
+          ),
         ),
       ),
     );
